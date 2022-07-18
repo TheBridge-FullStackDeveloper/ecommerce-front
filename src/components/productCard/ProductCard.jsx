b3487d698e3b39d5bdce18faf9cd1d70729fb657
Carlos Tutusaus <ctutusaus@connectingvisionsgroup.com>
2022-07-18 20:01:35 +0200

    Maring Rating

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -29,10 +29,10 @@ export default function ImgMediaCard() {
         El MacBook Pro de 13 pulgadas incluye el nuevo chip M2, que lo convierte en un titán. Ofrece hasta 20 horas de autonomía1 y un sistema de refrigeración activa para mantener un ritmazo incluso en tareas muy complejas. Y qué decir de la cámara FaceTime HD, la pantalla Retina y los micros con calidad de estudio. Además, su diseño compacto es ideal para trabajar donde sea. Tira millas.
         </Typography>
       </CardContent>
-      <CardActions sx={{display:"flex", justifyContent:"space-between", mr:3}}>
+      <CardActions sx={{display:"flex", justifyContent:"space-between", mr:2}}>
         <Button size="large" variant="contained" color="secondary" sx={{fontWeight:"bold", backgroundColor:"#7749F8",mb:0, '&:hover':{backgroundColor:"#FFFFFF", color:"#7749F8", fontWeight:'bold'} }}>Add to Cart</Button>
         <Box sx={{display:"flex"}}>
-            {Array(4)
+            {Array(5)
               .fill()
               .map((_, i)=>(
               <p>&#11088;</p>
